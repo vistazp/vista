e4008d2a42a2422f20902067be6cbbda509b6732
Vildtop <Vildtop@192.168.1.10>
2015-06-18 16:16:37 +0300

    post2

--- models/index_model.php
+++ models/index_model.php
@@ -6,7 +6,7 @@ class Index_model extends Model{
     }
 
     public function postList() {
-        return $this->db->select('select *, if (type = "standart", (TO_DAYS(CURRENT_TIMESTAMP())-TO_DAYS(date_pablish))*24*360+TIME_TO_SEC(CURRENT_TIMESTAMP())-TIME_TO_SEC(date_pablish), ((TO_DAYS(CURRENT_TIMESTAMP())-TO_DAYS(date_pablish))*24*360+TIME_TO_SEC(CURRENT_TIMESTAMP())-TIME_TO_SEC(date_pablish))-604800 ) as t2 from post where published="yes" order by t2;');
+        return $this->db->select('select *, if (type = "standart", (TO_DAYS(CURRENT_TIMESTAMP())-TO_DAYS(date_pablish))*24*3600+TIME_TO_SEC(CURRENT_TIMESTAMP())-TIME_TO_SEC(date_pablish), ((TO_DAYS(CURRENT_TIMESTAMP())-TO_DAYS(date_pablish))*24*3600+TIME_TO_SEC(CURRENT_TIMESTAMP())-TIME_TO_SEC(date_pablish))-604800 ) as t2 from post where published="yes" and TO_DAYS(CURRENT_TIMESTAMP())-TO_DAYS(date_pablish)<46 order by t2;');
    }
 }
     
