80c6fa0ae9ac9466ff86d765cd0265c5a57009d1
Vista-design <Vista-design@VistaDESIGN-PC>
2015-07-09 16:41:40 +0300

    платёжку типа сделали

--- controllers/payment.php
+++ controllers/payment.php
@@ -12,41 +12,37 @@ class payment extends controller {
         $this->view->titl = 'Select payment method';
         $this->view->render('error/index');
     }
+
     function view($postId) {
         $this->view->titl = 'Select payment method';
         $this->view->post = $this->model->singlePost($postId);
-         
-$micro = sprintf("%06d",(microtime(true) - floor(microtime(true))) * 1000000); // Ну раз что-то нужно добавить для полной уникализации то ..
-$number = date("YmdHis"); //Все вместе будет первой частью номера ордера
-$order_id = $number.$micro; //Будем формировать номер ордера таким образом...
- 
-$merchant_id='i92703554113'; //Вписывайте сюда свой мерчант
-$signature="HQHGNv3I91aRmEUs62j26Ubjkc9DM0b5dycWelH6"; //Сюда вносите public_key
- 
+
+        $micro = sprintf("%06d", (microtime(true) - floor(microtime(true))) * 1000000); // Ну раз что-то нужно добавить для полной уникализации то ..
+        $number = date("YmdHis"); //Все вместе будет первой частью номера ордера
+        $order_id = $number . $micro; //Будем формировать номер ордера таким образом...
+
+        $merchant_id = 'i92703554113'; //Вписывайте сюда свой мерчант
+        $signature = "HQHGNv3I91aRmEUs62j26Ubjkc9DM0b5dycWelH6"; //Сюда вносите public_key
 //$desc = $_GET['desc']; //Можно так принять назначение платежа
 //$order_id = $_GET['order_id']; //Можно так принять назначение платежа
-$price = $this->view->post[0]['price']; //Все что нужно скрипту - передать в него сумму (вы можете передавать все, вплоть до ордера и описания ...)
-$postToPay = $this->view->post[0]['postid']; 
-$liqpay = new LiqPay($merchant_id, $signature);
-$html = $liqpay->cnb_form(array(
- 'version' => '3',
- 'amount' => "$price",
- 'currency' => 'USD',     //Можно менять  'EUR','UAH','USD','RUB','RUR'
- 'description' => "Post №$postToPay",  //Или изменить на $desc
- 'order_id' => $order_id,
- 'language' =>'en',
- 'server_url' =>'http://webjobnow.com/feedback/callback',
- 'result_url' =>'http://webjobnow.com/thanks/success',
- 'sandbox' =>'1'
- ));
+        $price = $this->view->post[0]['price']; //Все что нужно скрипту - передать в него сумму (вы можете передавать все, вплоть до ордера и описания ...)
+        $postToPay = $this->view->post[0]['postid'];
+        $liqpay = new LiqPay($merchant_id, $signature);
+        $html = $liqpay->cnb_form(array(
+            'version' => '3',
+            //'amount' => "$price",
+            'amount' => "1",
+            'currency' => 'UAH', //Можно менять  'EUR','UAH','USD','RUB','RUR'
+            'description' => "Post №$postToPay", //Или изменить на $desc
+            'order_id' => $order_id,
+            'language' => 'en',
+            'server_url' => 'http://webjobnow.com/feedback/callback',
+            'result_url' => 'http://webjobnow.com/thanks/success'
+            //'sandbox' => '1'
+        ));
 
-$this->view->htm = $html;
-$this->view->render('payment/index');
+        $this->view->htm = $html;
+        $this->view->render('payment/index');
     }
-    
 
-    
 }
-
-
- 
